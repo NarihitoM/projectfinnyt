3a4e5069fb6090eb33ebe48e04369f5b760b4469
Narihito <heinboss234@gmail.com>
2026-04-06 21:27:13 +0630

    .

--- src/config/axiosconfig.ts
+++ src/config/axiosconfig.ts
@@ -2,6 +2,6 @@ import axios from "axios";
 
 //Server Backend Configure
 export const Server = axios.create({
-    baseURL : "https://finnyt-pj-server.vercel.app",
+    baseURL : "https://projectfinnyt-server.vercel.app/",
     withCredentials : true
 })
